1d85a4459fdbb6213a4d34b66ec7db36a129be18
DandiRizkyy <dandirizkyy94@gmail.com>
2023-09-02 22:25:11 +0700

    latihan php

---
+++ pertemuan2/index.php
@@ -0,0 +1,11 @@
+<?php
+// pengulangan
+// for($i = 0; $i < 10; $i++){
+//     var_dump("duar <br>");
+// }
+$i = 0;
+while($i < 10){
+    echo "ini pakai while <br>";
+    $i++;
+}
+?>
---
+++ pertemuan5/index.php
@@ -0,0 +1,4 @@
+<?php 
+    $name = ['dandi', 'doni'];
+    var_dump($name);
+?>
